4c77594405844eb30b3008cc1b0704a3d282e6f8
Iván Perdomo <ivan@perdomo.me>
2023-06-28 08:55:57 +0200

    [#1] Enable `hadolint`

--- ci/docker-build-push.sh
+++ ci/docker-build-push.sh
@@ -8,12 +8,15 @@ NAMESPACE="litefarm"
 PREFIX=$(date +%Y%m%d.%H%M%S)
 TAG="${PREFIX}.${CI_COMMIT:0:7}"
 
+docker pull hadolint/hadolint:latest-alpine
+
 while read -r change; do
 	(
 		echo "${change}"
 		if [[ "${change}" =~ Dockerfile$ ]]; then
 			image=$(awk -F '/' '{print $1}' <<<"${change}")
 			cd "${image}"
+            docker run --rm -i hadolint/hadolint:latest-alpine hadolint - < Dockerfile
 			docker build -t "${NAMESPACE}/${image}:${TAG}" .
 			docker push "${NAMESPACE}/${image}:${TAG}"
 		fi
